3665afa0adb22bece6784f5361dd13d46fe2ebaa
lgiesen <leo.richard.giesen@gmail.com>
2022-11-15 10:02:48 +0100

    :tada: add pug + homepage

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,12 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+div  
+  v-container
+    h1.center-items MensaToday
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  name: "Home",
+  components: {},
 };
 </script>
