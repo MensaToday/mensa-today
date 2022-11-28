7e46cab7a872a211cab5d63f139af9db59c8066d
martenjostmann <marten.jostmann@gmail.com>
2022-11-28 18:44:21 +0100

    #41 wrote logout function

--- mensa_recommend/api/urls.py
+++ mensa_recommend/api/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('get_data', views.getData),
     path('user/register', views.register),
+    path('user/logout', views.logout),
     path('user/check_card_id', views.check_card_id),
     path('mensa/get_dishplan', views.get_dishplan),
     path('mensa/user_ratings', views.user_ratings)
