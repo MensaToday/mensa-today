a15dd82b072b40df14b778a92441b8a388bb30fb
lgiesen <leo.richard.giesen@gmail.com>
2022-11-29 20:38:47 +0100

    :lipstick: visuals for login & card balance

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,36 @@
 <template lang="pug">
-  v-app
-    v-app-bar(app color='primary' dark)
-      .d-flex.align-center(to="/")
-        v-img.shrink.mr-2(alt='MensaToday Logo' contain src='@/assets/logo.png' transition='scale-transition' width='40')
-        h2 MensaToday
-      v-tabs(align-with-title)
-        v-spacer
-        v-tab.white--text(v-for="view in views" :key="view.to.name" :to="view.to") 
-          v-icon.mr-3 mdi-{{ view.icon }}
-          | {{ view.tag }}
-        v-spacer
-        .d-flex.align-center.mr-6
-          v-btn.px-3(outlined @click="logout()")
-            v-icon.mr-3 mdi-logout
-            | Logout
-    v-main
-      router-view
+v-app
+  v-app-bar(app color='primary' dark)
+    .d-flex.align-center(to="/")
+      v-img.shrink.mr-2(alt='MensaToday Logo' contain src='@/assets/logo.png' transition='scale-transition' width='40')
+      h2 MensaToday
+    v-tabs(align-with-title)
+      v-spacer
+      v-tab.white--text(v-for="view in views" :key="view.to.name" :to="view.to") 
+        v-icon.mr-3 mdi-{{ view.icon }}
+        | {{ view.tag }}
+      v-spacer
+      .d-flex.align-center.mr-6
+        v-icon.mr-2 mdi-wallet
+        p.my-auto.mr-9 3,49€
+        v-btn.px-3(outlined @click="logout()")
+          v-icon mdi-logout
+          | Logout
+  v-main
+    router-view
+    template
+  v-footer(dark padless)
+    v-card.indigo.lighten-1.white--text.text-center(flat tile)
+      v-card-text
+        v-btn.mx-4.white--text(v-for='icon in icons' :key='icon.mdi' icon target="_blank" :href="icon.link")
+          v-icon(size='24px' elevation='15')
+            | {{ icon.mdi }}
+      v-card-text.white--text.pt-0
+        | Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      v-divider
+      v-card-text.white--text
+        | {{ new Date().getFullYear() }} &mdash; 
+        strong Martin Jostmann, Leo Giesen, Erik Zimmermann, Marcel Reckmann, Polina Kireyeu
 </template>
 
 <script>
@@ -24,11 +39,6 @@ export default {
   name: 'App',
   data: () => ({
     views: [
-      // {
-      //   tag: "Home",
-      //   to: { name: "Home" },
-      //   icon: "home"
-      // },
       {
         tag: "Your Mensa Week",
         to: { name: "Home" },
@@ -45,6 +55,16 @@ export default {
         icon: "information-outline"
       }
     ],
+    icons: [
+      {
+        "mdi": "mdi-email",
+        "link": "mailto:leo.giesen@uni-muenster.de"
+      },
+      {
+        "mdi": "mdi-github",
+        "link": "https://github.com/erikzimmermann/data-integration-recommender"
+      },
+    ]
   }),
 };
 </script>
