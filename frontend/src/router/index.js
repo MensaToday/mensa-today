b01ce92d60b9fa559d20a74890f46e74cda1e28b
lgiesen <leo.richard.giesen@gmail.com>
2022-11-30 18:00:30 +0100

    #52 :lock: only allow authorized users to visit certain pages

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import store from "../store/index.js";
 import HomeView from "../views/HomeView.vue";
 
 Vue.use(VueRouter);
@@ -9,6 +10,7 @@ const routes = [
     path: "/",
     name: "Home",
     component: HomeView,
+    // check if user is logged in. If yes, redirect user to user-specific page
   },
   {
     path: "/about",
@@ -36,6 +38,7 @@ const routes = [
     name: "Suggestion",
     component: () =>
       import(/* webpackChunkName: "suggestion" */ "../views/Suggestion.vue"),
+    meta: { requiresAuth: true }
   }
 ];
 
@@ -43,6 +46,35 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+// check if a user is logged in and hence, is allowed to visit a page requiring authorization 
+router.beforeEach((to, from, next) => {
+  // step 1: if user logged in, redirect user to user-specific page
+  if(to.matched.some((record) => record.meta.isNotLoggedIn)) {
+    // if logged in, the user is redirected to the home page
+    if (store.getters.isLoggedIn) {
+      next('/')
+    }
+  }
+
+  // check if authorization is required: 
+  // cf. meta tags in routes
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // if a user is logged in, he can enter
+    if (store.getters.isLoggedIn) {
+      // and the correct user type is given
+      
+      if(to.matched.some((record) => record.meta.permissionLevel == store.getters.permissionLevelString)) {
+        next();
+        return;
+      }
+    }
+    // if not logged in, the user is redirected to the home page
+    next("/");
+  } else {
+    next();
+  }
 });
 
 export default router;
