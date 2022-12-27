3c65badaf66a878fd3f427e53ca502651110b61f
Leo Giesen <contact@leogiesen.de>
2022-12-27 18:10:22 +0100

    :rotating_light: linting settings caused error - potential fix

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  rules: [
-    {
-      enforce: 'pre',
-      test: /\.(js|vue)$/,
-      loader: 'eslint-loader',
-      exclude: /node_modules/
-    }
-  ],
-  lintOnSave: true
+  configureWebpack: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ],
+    lintOnSave: true
+  }
 });
