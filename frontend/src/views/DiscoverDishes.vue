8117c80a046f92c49effba5bd5d8f6317d1837c2
Leo Giesen <contact@leogiesen.de>
2023-01-17 21:27:15 +0100

    :bug: put mensa below food preferences to prevent card overflow

--- frontend/src/views/DiscoverDishes.vue
+++ frontend/src/views/DiscoverDishes.vue
@@ -65,40 +65,43 @@ div
                     v-card-title.subheading(style="word-break: normal")
                       | {{ item.dish.name }}
                     v-divider
-                    v-row
-                      v-col.align-center.justify-center.d-flex.justify-space-between.py-0
-                        h4.ma-0.text-right.subheading(:class="{'red--text': $store.state.card_balance <= (parseFloat(item.priceStudent)+1) }")
-                          //- (:class="{ 'primary--text': sortBy === key }")
-                          | €{{ item.priceStudent.replace('.',',') }}/{{ item.priceEmployee.replace('.',',') }}
-                        v-img(v-for="(category, index) in item.dish.categories.length" :alt="item.dish.categories[index].category.name" 
-                          height="50" max-width="50" contain :key="category"
-                          :src="require('@/assets/dish_icons/food_preferences/'+item.dish.categories[index].category.name+'.png')")
-                        v-btn(rounded :href="getGoogleMapsUrl(item.mensa.name)" target="_blank" rel="noopener noreferrer")
-                          v-icon mdi-navigation-variant-outline
-                          | {{ (item.mensa.name).replace('Bistro Katholische Hochschule', 'Bistro Katho.').replace('Bistro Oeconomicum','Oeconomicum') }}
-                    v-row  
-                      v-col
-                        span
-                          //- (:class="{ 'primary--text': sortBy === key }")
-                          v-icon mdi-food-apple
-                          | {{ item.dish.main ? 'Main Dish' : 'Side Dish' }}
-                    v-row 
-                      v-col    
-                        div 
-                          v-icon mdi-shield-plus-outline
+                    v-container.mt-2
+                      v-row
+                        v-col.align-center.justify-center.d-flex.justify-space-between.py-0
+                          h4.ma-0.text-right.subheading(:class="{'red--text': $store.state.card_balance <= (parseFloat(item.priceStudent)+1) }")
+                            //- (:class="{ 'primary--text': sortBy === key }")
+                            | €{{ item.priceStudent.replace('.',',') }}/{{ item.priceEmployee.replace('.',',') }}
+                          div.d-flex
+                            v-img(v-for="(category, index) in item.dish.categories.length" :alt="item.dish.categories[index].category.name" 
+                              height="50" max-width="50" contain :key="category"
+                              :src="require('@/assets/dish_icons/food_preferences/'+item.dish.categories[index].category.name+'.png')")
+                      v-row  
+                        v-col
                           span
                             //- (:class="{ 'primary--text': sortBy === key }")
-                            span(v-if="item.dish.additives.length == 0")  None
-                            span(v-for="additive in item.dish.additives" :key="additive.additive.name") 
-                              span {{ additive.additive.name }}
-                              span(v-show="additive != item.dish.additives[item.dish.additives.length-1]") , 
-                    v-row 
-                      v-col.align-center.justify-center.d-flex.justify-space-between
-                        div 
-                          v-icon mdi-calendar
-                          span {{ new Date(item.date).toLocaleDateString('de-DE') }}
-                        v-rating(hover length="5" background-color="gray" readonly size="24" half-increments 
-                          v-model="parseFloat(item.ext_ratings.rating_avg) + 0.0")
+                            v-icon mdi-food-apple
+                            | {{ item.dish.main ? 'Main Dish' : 'Side Dish' }}
+                        v-col 
+                          v-btn(rounded :href="getGoogleMapsUrl(item.mensa.name)" target="_blank" rel="noopener noreferrer")
+                            v-icon mdi-navigation-variant-outline
+                            | {{ (item.mensa.name).replace('Bistro Katholische Hochschule', 'Bistro Katho.').replace('Bistro Oeconomicum','Oeconomicum') }}
+                      v-row 
+                        v-col    
+                          div 
+                            v-icon mdi-shield-plus-outline
+                            span
+                              //- (:class="{ 'primary--text': sortBy === key }")
+                              span(v-if="item.dish.additives.length == 0")  None
+                              span(v-for="additive in item.dish.additives" :key="additive.additive.name") 
+                                span {{ additive.additive.name }}
+                                span(v-show="additive != item.dish.additives[item.dish.additives.length-1]") , 
+                      v-row 
+                        v-col.align-center.justify-center.d-flex.justify-space-between
+                          div 
+                            v-icon mdi-calendar
+                            span {{ new Date(item.date).toLocaleDateString('de-DE') }}
+                          v-rating(hover length="5" background-color="gray" readonly size="24" half-increments 
+                            v-model="parseFloat(item.ext_ratings.rating_avg) + 0.0")
 
             template(v-slot:footer)
               v-row.mt-2(align='center' justify='center')
